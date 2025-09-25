8335c7bf08edcd5e8b738f706b6dbdf08b407377
roman.akbashev <roman.akbashev@infotecs.ru>
2025-09-25 23:14:18 +0500

    Перейти с коллбеков на саспенд функции и корутины

--- hw-02-coroutines/app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ hw-02-coroutines/app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,8 +1,12 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import java.net.SocketTimeoutException
 
 class CatsPresenter(
     private val catsService: CatsService
@@ -10,19 +14,23 @@ class CatsPresenter(
 
     private var _catsView: ICatsView? = null
 
+    private val presenterScope = CoroutineScope(
+        Dispatchers.Main + SupervisorJob() + CoroutineName("CatsCoroutine")
+    )
+
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
-            }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
+        presenterScope.launch {
+            try {
+                val fact = catsService.getCatFact()
+                _catsView?.populate(fact)
+            } catch (_: SocketTimeoutException) {
+                _catsView?.showToast("Не удалось получить ответ от сервера")
+            } catch (e: Exception) {
                 CrashMonitor.trackWarning()
+                _catsView?.showToast(e.message ?: "Произошла ошибка")
             }
-        })
+
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -31,5 +39,6 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+        presenterScope.cancel()
     }
 }
