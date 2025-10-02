9a4901607d65f0de985f29edb4e69d879e93095c
roman.akbashev <roman.akbashev@infotecs.ru>
2025-10-03 00:08:31 +0500

    Реализовать функции с использование flow операторов

--- hw-03-flow/operators/src/main/java/otus/homework/flow/SampleInteractor.kt
+++ hw-03-flow/operators/src/main/java/otus/homework/flow/SampleInteractor.kt
@@ -1,7 +1,16 @@
 package otus.homework.flow
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.zip
 
 @ExperimentalCoroutinesApi
 class SampleInteractor(
@@ -18,7 +27,12 @@ class SampleInteractor(
      * 6) возвращает результат
      */
     fun task1(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .map { it * 5 }
+            .filterNot { it <= 20 }
+            .filterNot { it % 2 == 0 }
+            .map { "$it won" }
+            .take(3)
     }
 
     /**
@@ -28,9 +42,20 @@ class SampleInteractor(
      * Если входное число делится на 15 - эмитим само число и после него эмитим строку FizzBuzz
      * Если число не делится на 3,5,15 - эмитим само число
      */
+    @OptIn(FlowPreview::class)
     fun task2(): Flow<String> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .flatMapConcat {
+                val number = it.toString()
+                when {
+                    it % 15 == 0 -> flowOf(number, "FizzBuzz")
+                    it % 3 == 0 -> flowOf(number, "Fizz")
+                    it % 5 == 0 -> flowOf(number, "Buzz")
+                    else -> flowOf(number)
+                }
+            }
     }
+
 
     /**
      * Реализуйте функцию task3, которая объединяет эмиты из двух flow и возвращает кортеж Pair<String,String>(f1,f2),
@@ -38,7 +63,10 @@ class SampleInteractor(
      * Если айтемы в одно из флоу кончились то результирующий флоу также должен закончится
      */
     fun task3(): Flow<Pair<String, String>> {
-        return flowOf()
+        return sampleRepository.produceColors()
+            .zip(sampleRepository.produceForms()) { color, form ->
+                Pair(color, form)
+            }
     }
 
     /**
@@ -48,6 +76,13 @@ class SampleInteractor(
      * При любом исходе, будь то выброс исключения или успешная отработка функции вызовите метод dotsRepository.completed()
      */
     fun task4(): Flow<Int> {
-        return flowOf()
+        return sampleRepository.produceNumbers()
+            .catch {
+                if (it is IllegalArgumentException) emit(-1)
+                else throw it
+            }
+            .onCompletion {
+                sampleRepository.completed()
+            }
     }
 }
