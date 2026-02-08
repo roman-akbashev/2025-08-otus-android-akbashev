09982a1644dd6751c43fb10ed978cf477957d829
roman.akbashev <roman.akbashev@infotecs.ru>
2026-02-08 20:49:51 +0500

    Создал проект, добавил слой данных

---
+++ hw-08-persistent-storage/app/src/main/java/ru/otus/roomapp/data/NoteDao.kt
@@ -0,0 +1,28 @@
+package ru.otus.roomapp.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM notes ORDER BY createdAt DESC")
+    fun getAllNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+}
---
+++ hw-08-persistent-storage/app/src/main/java/ru/otus/roomapp/data/NoteRepository.kt
@@ -0,0 +1,20 @@
+package ru.otus.roomapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insertNote(note: Note) {
+        noteDao.insertNote(note)
+    }
+
+    suspend fun updateNote(note: Note) {
+        noteDao.updateNote(note)
+    }
+
+    suspend fun deleteNote(note: Note) {
+        noteDao.deleteNote(note)
+    }
+}
